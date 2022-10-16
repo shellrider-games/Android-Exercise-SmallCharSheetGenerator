3d1930876e449db6bb50c0f8fad0a610593e35c7
Georg Becker <georg.becker@shellrider.com>
2022-10-16 14:32:36 +0300

    Start Class and name selection

---
+++ android-project/app/src/main/java/com/shellrider/simplecharsheetgenerator/Charsheet.kt
@@ -0,0 +1,11 @@
+package com.shellrider.simplecharsheetgenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Charsheet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_charsheet)
+    }
+}
--- android-project/app/src/main/java/com/shellrider/simplecharsheetgenerator/MainActivity.kt
+++ android-project/app/src/main/java/com/shellrider/simplecharsheetgenerator/MainActivity.kt
@@ -2,10 +2,37 @@ package com.shellrider.simplecharsheetgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.util.Log
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpClassSpinner()
+    }
+
+    private fun setUpClassSpinner() {
+        val characterClasses = resources.getStringArray(R.array.characterClasses)
+        val spinner = findViewById<Spinner>(R.id.classSelectSpinner)
+
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                R.layout.spinner_list, characterClasses)
+            adapter.setDropDownViewResource(R.layout.spinner_list)
+            spinner.adapter = adapter
+        }
+    }
+
+    fun generateCharsheet(view: View){
+        val characterName = findViewById<EditText>(R.id.editTextCharacterName).text.toString()
+        val selectedCharacterClass = findViewById<Spinner>(R.id.classSelectSpinner).selectedItemId.toInt()
+        val selectedClassName = resources.getStringArray(R.array.characterClasses)[selectedCharacterClass]
+        Log.d("GenerateCharsheet", "$characterName - $selectedClassName");
     }
 }
